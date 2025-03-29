a36d37d25779c42c317621e12f84adf4846e9e3d
Stephen Villejo <stephen.villejo@glasgow.ac.uk>
2025-03-29 01:13:58 +0000

    correction on the initial values

--- rgeneric_functions.R
+++ rgeneric_functions.R
@@ -83,9 +83,7 @@ rgeneric.Beta.midas = function(cmd = c("graph", "Q", "mu", "initial", "log.norm.
     }
   }
   initial = function() { 
-    par = interpret.theta()
-    n.params <- length(par)
-    return(rep(1, n.params+1))
+    return(rep(1, 2))
   }
   quit = function() { 
     return(invisible())
@@ -97,9 +95,10 @@ rgeneric.Beta.midas = function(cmd = c("graph", "Q", "mu", "initial", "log.norm.
 
 
 
-rgeneric.Almon.midas = function(cmd = c("graph", "Q", "mu", "initial", "log.norm.const",
+rgeneric.Almon2.midas = function(cmd = c("graph", "Q", "mu", "initial", "log.norm.const",
                                         "log.prior", "quit"),
-                                theta = NULL){
+                                 theta = NULL){
+  
   envir = parent.env(environment())
   ## artificial high precision to be added to the mean-model
   prec.high = exp(15)
@@ -180,9 +179,7 @@ rgeneric.Almon.midas = function(cmd = c("graph", "Q", "mu", "initial", "log.norm
     }
   }
   initial = function() { 
-    par = interpret.theta()
-    n.params <- length(par)
-    return(rep(1, n.params+1))
+    return(rep(1, 3))
   }
   quit = function() { 
     return(invisible())
@@ -192,6 +189,101 @@ rgeneric.Almon.midas = function(cmd = c("graph", "Q", "mu", "initial", "log.norm
   return(val)
 }
 
+
+rgeneric.Almon3.midas = function(cmd = c("graph", "Q", "mu", "initial", "log.norm.const",
+                                         "log.prior", "quit"),
+                                 theta = NULL){
+  
+  envir = parent.env(environment())
+  ## artificial high precision to be added to the mean-model
+  prec.high = exp(15)
+  
+  interpret.theta = function() {
+    
+    lag_k <- ncol(x)-2
+    gamma1 <- 0.01*sin(theta[2L])
+    gamma2 <- 0.01*sin(theta[3L])
+    gamma3 <- 0.01*sin(theta[4L])
+    for(lag in 0:lag_k){
+      temp <- exp(gamma1*(lag^1) + gamma2*(lag^2) + gamma3*(lag^3))
+      assign(paste0("psi",lag), temp)
+    }
+    
+    compile_sum <- 0
+    for(lag in 0:lag_k){
+      compile_sum <- compile_sum + get(paste0("psi", lag))
+    }
+    
+    for(lag in 0:lag_k){
+      temp <- get(paste0("psi",lag))
+      assign(paste0("w",lag),temp/compile_sum)
+    }
+    
+    out_list <- vector(mode = "list", length = 1 + lag_k + 1)
+    out_list[[1]] <- theta[1L]
+    for(lag in 0:lag_k){
+      out_list[[lag+2]] <- get(paste0("w",lag))
+    }
+    
+    compile_names <- c()
+    for(lag in 0:lag_k){
+      compile_names <- c(compile_names, paste0("w",lag))
+    }
+    names_vec <- c("beta1", compile_names)
+    names(out_list) <- names_vec
+    return(out_list)
+    
+  }
+  graph = function() {
+    G = Diagonal(n = length(x$lag0), x=1) 
+    return(G)
+  }
+  Q = function() {
+    Q = prec.high * graph() 
+    return(Q)
+  }
+  mu = function() {
+    par = interpret.theta() 
+    
+    lag_k = ncol(x)-2
+    compile_lag_label <- c()
+    for(lag in 0:lag_k){
+      compile_lag_label <- c(compile_lag_label, paste0("lag",lag))
+    }
+    compile_w_label <- c()
+    for(lag in 0:lag_k){
+      compile_w_label <- c(compile_w_label, paste0("w",lag))
+    }
+    
+    agg <- 0
+    for(lag in 0:lag_k){
+      agg <- agg + par[[compile_w_label[[lag+1]]]] * x[,which(names(x) == compile_lag_label[[lag+1]])]
+    }
+    
+    return(par$beta1 * agg)
+  }
+  log.norm.const = function() { 
+    return(numeric(0))
+  }
+  log.prior = function() {
+    par = interpret.theta()
+    n.params <- length(par)
+    val <- 0
+    for(i in 1:n.params){
+      temp <- dnorm(par[[i]], mean=0, sd=1, log=TRUE)
+      val <- val + temp
+    }
+  }
+  initial = function() { 
+    return(rep(1, 4))
+  }
+  quit = function() { 
+    return(invisible())
+  }
+  
+  val = do.call(match.arg(cmd), args = list())
+  return(val)
+}
 
 
 rgeneric.Hyperbolic.midas = function(cmd = c("graph", "Q", "mu", "initial", "log.norm.const",
@@ -276,9 +368,7 @@ rgeneric.Hyperbolic.midas = function(cmd = c("graph", "Q", "mu", "initial", "log
     }
   }
   initial = function() { 
-    par = interpret.theta()
-    n.params <- length(par)
-    return(rep(1, n.params+1))
+    return(rep(1, 2))
   }
   quit = function() { 
     return(invisible())
